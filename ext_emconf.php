e6eccf3b5c501f3e370287a13bdd9ec73b58ddd6
Gregor Agnes <ga@zwo3.de>
2021-09-01 12:51:07 +0200

    feat: Mask 7.0 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF['mask_kesearch_indexer'] = array(
     'title' => 'KeSearch Indexer for Mask Elements',
     'description' => 'Indexer for mask elements, both tt_content columns and tx_mask-tables.',
     'category' => 'backend',
-    'version' => '1.6.4',
+    'version' => '1.6.5',
     'dependencies' => 'ke_search, mask',
     'state' => 'stable',
     'author' => 'Gregor Agnes',
@@ -13,7 +13,7 @@ $EM_CONF['mask_kesearch_indexer'] = array(
     'constraints' => array(
         'depends' => array(
             'typo3' => '9.5.0-10.4.99',
-            'mask' => '4.1.2-6.99.99',
+            'mask' => '4.1.2-7.0.99',
             'ke_search' => '3.1.0-3.99.99',
         ),
         'conflicts' => array(),
